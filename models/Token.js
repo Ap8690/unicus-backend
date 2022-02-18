0f4b97fd77a4c45ff9be830e6341f3f353df7def
Karan-Dhingra <Dhingrakaran764@gmail.com>
2022-02-18 19:55:39 +0530

    update admin controll

--- models/Token.js
+++ models/Token.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const TokenSchema = new mongoose.Schema(
-  {
-    token: { type: String, required: true },
-    ip: { type: String, required: true },
-    userAgent: { type: String, required: true },
-    isValid: { type: Boolean, default: true },
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: 'User',
-      required: true,
+    {
+        token: { type: String, required: true },
+        ip: { type: String, required: true },
+        userAgent: { type: String, required: true },
+        isValid: { type: Boolean, default: true },
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+        },
+        admin: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Admin',
+        },
     },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+)
 
-module.exports = mongoose.model('Token', TokenSchema);
+module.exports = mongoose.model('Token', TokenSchema)
--- utils/sendVerficationEmail.js
+++ utils/sendVerficationEmail.js
@@ -1,24 +1,23 @@
-const sendEmail = require("./sendEmail");
+const sendEmail = require('./sendEmail')
 
 const sendVerificationEmail = async ({
-  name,
-  email,
-  verificationToken,
-  origin,
+    name,
+    email,
+    verificationToken,
+    origin,
 }) => {
-  const verifyEmail = `https://marketplace.unicus.one/login/${verificationToken}/${email}`;
+    const verifyEmail = `https://marketplace.unicus.one/login/${verificationToken}/${email}`
 
-  const message = `<p>Please confirm your email by clicking on the following link : 
-  <a href="${verifyEmail}">Verify Email</a> </p>`;
+    const message = `<p>Please confirm your email by clicking on the following link : 
+  <a href="${verifyEmail}">Verify Email</a> </p>`
 
-  return sendEmail({
-    to: email,
-    subject: "Email Confirmation",
-    html: `<h4> Hello ${name.charAt(0).toUpperCase() + name.slice(1)},</h4>
+    return sendEmail({
+        to: email,
+        subject: 'Email Confirmation',
+        html: `<h4> Hello ${name.charAt(0).toUpperCase() + name.slice(1)},</h4>
     ${message}
     `,
-  });
-};
+    })
+}
 
-module.exports = sendVerificationEmail;
-
+module.exports = sendVerificationEmail
