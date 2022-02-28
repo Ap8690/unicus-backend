6b4338457843d0e2cc6147c0ca629e41e3dfd9a9
Nayan96 <nnayanamin96@gmail.com>
2022-02-28 12:43:34 +0530

    middleware

--- middleware/domain-parser.js
+++ middleware/domain-parser.js
@@ -1,7 +1,7 @@
 const { Storefront } = require("../models");
 
 const domainParser = async (req, res, next) => {
-  const url = req.get('origin')
+  const url = req.get('host')
   const subdomain = req.subdomain
   // const storefront = await Storefront.find({})
   console.log("url", url, subdomain);
