0f27963aa70aae18f5343ae12e4d7bfee97f21c5
Nayan96 <nnayanamin96@gmail.com>
2022-03-29 13:45:44 +0530

    auth-fix

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,16 +15,26 @@ const createLimitedTimeToken = ({ payload, expiresIn }) => {
   return token;
 };
 
-const isTokenValid = (token) => {
-  try {
-    console.log("valid", token, process.env.JWT_SECRET);
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return decoded;
-  } catch (err) {
-    return false;
-  }
-};
+// const isTokenValid = (token) => {
+//   try {
+//     console.log("valid", token, process.env.JWT_SECRET);
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     return decoded;
+//   } catch (err) {
+//     return false;
+//   }
+// };
+const isTokenValid = (token) =>{
+  try{
+   const decoded = jwt.verify(token, String(process.env.JWT_SECRET))
+   return decoded
+}
+catch(err){
+  console.log(err);
+  return false;
+}
 
+}
 module.exports = {
   createJWT,
   isTokenValid,
