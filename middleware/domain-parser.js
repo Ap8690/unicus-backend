af1e9dfad0da6df9fa364aac3e5d09bea607c7aa
Nayan96 <nnayanamin96@gmail.com>
2022-04-27 10:31:20 +0530

    deploy-2

--- middleware/domain-parser.js
+++ middleware/domain-parser.js
@@ -6,7 +6,7 @@ const domainParser = async (req, res, next) => {
   try{
   const url = req.header("Origin")
   const { subdomain, domain, hostname } = parse(url);
-  console.log("host",req.header, hostname, domain, process.env.NODE_ENV);
+  console.log("host",req, hostname, domain, process.env.NODE_ENV);
   if(process.env.NODE_ENV !=="local"){
     if(domain != "unicus.one" && domain!= "herokuapp.com"){
       throw new CustomError.BadRequestError("Invalid Website");
