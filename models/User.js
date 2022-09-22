70537c964134429fc9f40d784eb6b2d2477589f3
shrishtieth <apple@Apples-MacBook-Pro.local>
2022-09-22 17:38:18 +0530

    70

--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       unique: [true, "username has already taken"],
       minlength: [3, "Username should have at least 3 characters"],
-      maxlength: [60, "Username cannot have more than 15 characters"],
+      maxlength: [70, "Username cannot have more than 70 characters"],
     },
     email: {
       type: String,
