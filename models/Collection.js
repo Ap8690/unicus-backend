08bcccd817bdfc3671ec782141a436778098717a
Uddeshya Tiwari <uddeshyatiwari9085@gmail.com>
2022-10-17 01:40:02 +0530

    adding createCollection api

--- models/Collection.js
+++ models/Collection.js
@@ -9,7 +9,30 @@ const CollectionSchema = new mongoose.Schema(
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
-
+    logoUrl: {
+      type: String
+    },
+    bannerUrl: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    category: {
+      type: String
+    },
+    websiteUrl: {
+      type: String
+    },
+    twitterUrl: {
+      type: String
+    },
+    discordUrl: {
+      type: String
+    },
+    telegramUrl: {
+      type: String
+    },
     storefront: {
       type: mongoose.Types.ObjectId,
       ref: "Storefront",
